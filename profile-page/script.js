0bcb6adc7234c06df45a1ba565479bda0b5b4eb6
NEHWILLETANYE <nehwillet673@gmail.com>
2026-07-30 08:27:04 -0700

    Add script.js with style.css to index.html to create a profile page

---
+++ profile-page/script.js
@@ -0,0 +1,9 @@
+function toggleAbout() {
+    const section = document.getElementById("about");
+
+    if (section.style.display === "none"){
+        section.style.display = "block";
+    }else{
+        section.style.display = "none";
+    }
+}
